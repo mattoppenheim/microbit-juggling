7d527aa7ddc2c7e5ed3da15b854caf65dcb946b8
hardwaremonkey <matt.oppenheim@gmail.com>
2018-05-16 13:37:15 +0100

    real time graphing with pyqtgraph using simulated data

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ Data samples are held in numpy arrays.
 Matthew Oppenheim May 2018. '''
 
 
-
 import PySide
 from pyqtgraph.Qt import QtGui, QtCore
 import pyqtgraph as pg
@@ -23,7 +22,7 @@ NUM_SAMPLES = 300
 # frequence to refresh the display
 SCREEN_REFRESH_RATE = 15
 
-class MicrobitJuggling():
+class MicrobitJuggle():
     def __init__(self):
         self.app = QtGui.QApplication([])
         mb1 = self.initialise_data()
@@ -39,7 +38,7 @@ class MicrobitJuggling():
         win.nextRow()
         p3 = win.addPlot()
         for plot in [p1, p2, p3]:
-            plot.setYRange(-3,3)
+            plot.setYRange(0,3000)
         self.text = pg.TextItem('text', anchor=(0,3))
         p3.addItem(self.text)
         self.curve1 = p1.plot()
@@ -50,10 +49,25 @@ class MicrobitJuggling():
         self.freq = None
         self.freq_list = np.zeros(FREQ_AVG)
 
+
+    def graph_update_rate(self):
+            ''' Calculate graph refresh frequency. '''
+            now_time = time()
+            dt = now_time - self.last_time
+            self.last_time = now_time
+            if self.freq is None:
+                self.freq = 1.0/dt
+            else:
+                s = np.clip(dt*3., 0, 1)
+                self.freq = self.freq * (1-s) + (1.0/dt) * s
+            self.freq_list[-1] = self.freq
+            return(FREQ_AVG*np.average(self.freq_list))
+
+
     def initialise_data(self):
         ''' Create a numpy arrays of 0s. '''
         # MAKE dtype INT
-        return np.zeros(NUM_SAMPLES,dtype=float)
+        return np.zeros(NUM_SAMPLES,dtype=int)
 
     def roll_data(self, data):
         ''' Rolls <data> by one sample. '''
@@ -63,18 +77,9 @@ class MicrobitJuggling():
 
     def update(self):
         ''' Update the plot curves with a new value. SYNTHETIC DATA '''
-        now_time = time()
-        dt = now_time - self.last_time
-        self.last_time = now_time
-        if self.freq is None:
-            self.freq = 1.0/dt
-        else:
-            s = np.clip(dt*3., 0, 1)
-            self.freq = self.freq * (1-s) + (1.0/dt) * s
-        self.freq_list[-1] = self.freq
         for data in self.mb_data:
-            data[-1] = np.random.normal()
-        self.text.setText('self.frequency: {:0.1f}'.format(FREQ_AVG*np.average(self.freq_list), color=(255,255,0)))
+            data[-1] = int(abs(np.random.normal(scale=1000)))
+        self.text.setText('self.frequency: {:0.1f}'.format(self.graph_update_rate(), color=(255,255,0)))
         self.curve1.setData(self.mb_data[0])
         self.curve2.setData(self.mb_data[1])
         self.curve3.setData(self.mb_data[2])
@@ -82,13 +87,13 @@ class MicrobitJuggling():
             data = self.roll_data(data)
         else:
             self.index+=1
-        self.app.processEvents()
+        # self.app.processEvents()
 
 
 
 if __name__ == '__main__':
-    print('instaiting microbit_juggling')
-    microbit_juggling = MicrobitJuggling()
+    print('instantiating MicrobitJuggle ')
+    microbit_juggling = MicrobitJuggle()
     timer = QtCore.QTimer()
     timer.timeout.connect(microbit_juggling.update)
     # timer units are milliseconds. timer.start(0) to go as fast as practical.
